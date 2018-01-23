3d6c177ed894fa2bcc2cb2a51c27b1411ee3e46b
Joe Begley <joebegley@Joes-MacBook-Air.local>
2018-01-23 15:30:08 -0500

    Node server can now load basic HTML file

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 var http = require('http');
 var portNum = 8080;
+var fs = require('fs');
+var index = fs.readFileSync('index.html');
+
 http.createServer(function(request,response){
-	response.writeHead(200);
-	response.write('I am a working server');
-	response.end();
+	response.writeHead(200, {'Content-Type': 'html'});
+  	response.end(index);
 }).listen(portNum, function(){
-	console.log('Server is up and running on port #' + portNum);
+	console.log('Server is up and running on port #' + portNum); 
 });
